e49c430166af9c795a2c42a615f3d51a4ca9d7f7
Oliver <oli67@posteo.de>
2022-02-17 23:29:35 +0100

    Init

---
+++ Fahrenheit.py
@@ -0,0 +1,8 @@
+print (" Hallo beim Temparatur converter!")
+
+fahrenheit = imput("Bitte gib die Temperatur in Fahrenheit an:")
+print ("Du hast " +fahrenheit + "° Fahrenheit angegeben.")
+
+fahrenheitFloat = float(fahrenheit)
+celsius = (fahrenheitFloat -32) * 5/9
+print ("")
